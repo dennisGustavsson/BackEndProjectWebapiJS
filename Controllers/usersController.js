36602413b6d03bdf1512d74e8f615303eb579727
Dennis Gustavsson <58669253+dennisGustavsson@users.noreply.github.com>
2022-11-30 19:24:06 +0100

    adjusted for products

--- Controllers/usersController.js
+++ Controllers/usersController.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const controller = express.Router();
-let users = require("../Data/simulated_database");
+let products = require("../Data/simulated_database");
 
 //! middlware
-controller.param("id", (req, res, next, id) => {
-	req.user = users.find((user) => user.id == id);
+controller.param("articleNumber", (req, res, next, articleNumber) => {
+	req.product = products.find(
+		(product) => product.articleNumber == articleNumber
+	);
 	next();
 });
 
@@ -15,53 +17,62 @@ controller.param("id", (req, res, next, id) => {
 controller
 	.route("/")
 	.post((req, res) => {
-		let user = {
-			id: users[users.length - 1]?.id > 0 ? users[users.length - 1]?.id + 1 : 1, //!checks index of users-list, if id < 0 increment 1 on id
-			firstName: req.body.firstName,
-			lastName: req.body.lastName,
-			email: req.body.email,
-			password: req.body.password,
+		let product = {
+			articleNumber:
+				products[products.length - 1]?.articleNumber > 0
+					? products[products.length - 1]?.articleNumber + 1
+					: 1, //!checks index of users-list, if id < 0 increment 1 on id
+			name: req.body.name,
+			category: req.body.category,
+			rating: req.body.rating,
+			price: req.body.price,
+			description: req.body.description,
 		};
-		users.push(user); //! pushes the user up to users-list
-		res.status(201).json(user); //! sends back CREATED code and user in json format
+		products.push(product); //! pushes the user up to users-list
+		res.status(201).json(product); //! sends back CREATED code and user in json format
 	})
 	//!get all
 	.get((req, res) => {
-		res.status(200).json(users);
+		res.status(200).json(products);
 	});
 
 //! GET ONE - READ - http://localhost:5000/api/users/id
 
 controller
-	.route("/:id")
+	.route("/:articleNumber")
 	.get((req, res) => {
-		if (req.user != undefined) {
-			console.log(req.user);
-			res.status(200).json(req.user);
+		if (req.product != undefined) {
+			res.status(200).json(req.product);
 		} else {
 			res.status(404).json();
 		}
 	})
 	.put((req, res) => {
-		if (req.user != undefined) {
-			users.forEach((user) => {
-				if (user.id == req.user.id) {
-					user.firstName = req.body.firstName
-						? req.body.firstName
-						: user.firstName;
-					user.lastName = req.body.lastName ? req.body.lastName : user.lastName;
-					user.email = req.body.email ? req.body.email : user.email;
+		if (req.product != undefined) {
+			products.forEach((product) => {
+				if (product.articleNumber == req.product.articleNumber) {
+					product.name = req.body.name ? req.body.name : product.name;
+					product.category = req.body.category
+						? req.body.category
+						: product.category;
+					product.price = req.body.price ? req.body.price : product.price;
+					product.rating = req.body.rating ? req.body.rating : product.rating;
+					product.description = req.body.description
+						? req.body.description
+						: product.description;
 				}
 			});
-			res.status(200).json(req.user);
+			res.status(200).json(req.product);
 		} else {
 			res.status(404).json();
 		}
 	})
 	.delete((req, res) => {
-		if (req.user != undefined) {
-            users = users.filter(user => user.id !== req.user.id)
-            res.status(204).json()
+		if (req.product != undefined) {
+			products = products.filter(
+				(product) => product.articleNumber !== req.product.articleNumber
+			);
+			res.status(204).json();
 		} else {
 			res.status(404).json();
 		}
